3afce3d3f7c85cb83c2ba97288f1b3181b177a5f
Huang Yunkun <htynkn@gmail.com>
2019-01-29 22:38:27 +0800

    update deploy script for maven

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 echo "Processing" + $1
 node deploy.js $1
 cd $1
-npm install
+if [ -e "package.json" ]; then
+    npm install
+fi
+if [ -e "pom.xml" ]; then
+    ./mvnw clean package
+fi
 fun deploy
