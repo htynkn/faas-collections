20ff29d0fd75a1cf5bce71a4d255de126dca62cd
Huang Yunkun <htynkn@gmail.com>
2019-01-29 22:39:37 +0800

    init java faas

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,11 @@ echo "Processing" + $1
 node deploy.js $1
 cd $1
 if [ -e "package.json" ]; then
+    echo "node project"
     npm install
 fi
 if [ -e "pom.xml" ]; then
+    echo "java maven project"
     ./mvnw clean package
 fi
 fun deploy
