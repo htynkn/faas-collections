0572e6cd97fb12ba7690ec976e630396b183fc90
Huang Yunkun <htynkn@gmail.com>
2019-07-21 21:05:56 +0800

    find parent by api

--- update-forked-repo/index.js
+++ update-forked-repo/index.js
@@ -1,65 +1,105 @@
 var request = require("request");
 var async = require("async");
 
-var targets = {
-    "htynkn/spark": "apache/spark",
-    "htynkn/dubbo": "apache/dubbo",
-    "htynkn/spring-boot": "spring-projects/spring-boot",
-    "htynkn/fun": "aliyun/fun"
-};
+var targets = ["htynkn/spark"];
 
-module.exports.handler = function (event, context, callback) {
-    var options = {
-        headers: {
-            "User-Agent": "update-forked-repo",
-            "Authorization": "token " + process.env.GITHUB_TOKEN
-        }
-    };
-    var execResult = [];
-    async.forEachOf(targets, function (value, key, eachCallback) {
-        async.waterfall([
-            function (cb) {
-                request.get("https://api.github.com/repos/" + value + "/branches/master", options, function (error, response, body) {
-                    if (error) {
-                        cb(error)
-                    }
-                    var sha = JSON.parse(body).commit.sha;
-                    cb(null, sha);
+module.exports.handler = function(event, context, callback) {
+  var options = {
+    headers: {
+      "User-Agent": "update-forked-repo",
+      Authorization: "token " + process.env.GITHUB_TOKEN
+    }
+  };
+  var execResult = [];
+  async.each(
+    targets,
+    function(value, eachCallback) {
+      async.waterfall(
+        [
+          function(cb) {
+            request.get(
+              "https://api.github.com/repos/" + value,
+              options,
+              function(error, response, body) {
+                if (error) {
+                  cb(error);
                 }
-                )
-            },
-            function (sha, cb) {
-                request.patch("https://api.github.com/repos/" + key + "/git/refs/heads/master", {
-                    headers: options.headers,
-                    json: {
-                        "sha": sha,
-                        "force": false
-                    }
-                }, function (error, response, body) {
-                    if (error) {
-                        cb(error)
-                    }
-                    cb(null, body);
-                })
-            }], function (err, result) {
-                if (err) {
-                    eachCallback(err)
-                } else {
-                    execResult.push({
-                        target: key,
-                        time: new Date(),
-                        result: result.object,
-                    });
-                    eachCallback();
+                const responseAsJson = JSON.parse(body);
+                var defaultBranch = responseAsJson.default_branch;
+                if (response.parent != null) {
+                  var parentName = responseAsJson.parent.full_name;
+                  cb(null, {
+                    defaultBranch: defaultBranch,
+                    parentName: parentName
+                  });
                 }
-            }
-        );
-    }, function (err) {
-        if (err) {
-            callback(err)
-        } else {
-            console.log(execResult);
-            callback(null, execResult);
+                cb("No parent, skip");
+              }
+            );
+          },
+          function(info, cb) {
+            console.log(info);
+            request.get(
+              "https://api.github.com/repos/" +
+                info.parentName +
+                "/branches/" +
+                info.defaultBranch,
+              options,
+              function(error, response, body) {
+                if (error) {
+                  cb(error);
+                }
+                var sha = JSON.parse(body).commit.sha;
+                cb(null, {
+                  defaultBranch: info.defaultBranch,
+                  sha: sha
+                });
+              }
+            );
+          },
+          function(info, cb) {
+            request.patch(
+              "https://api.github.com/repos/" +
+                value +
+                "/git/refs/heads/" +
+                info.defaultBranch,
+              {
+                headers: options.headers,
+                json: {
+                  sha: info.sha,
+                  force: false
+                }
+              },
+              function(error, response, body) {
+                if (error) {
+                  cb(error);
+                }
+                cb(null, body);
+              }
+            );
+          }
+        ],
+        function(err, result) {
+          if (err) {
+            eachCallback(err);
+          } else {
+            execResult.push({
+              target: value,
+              time: new Date(),
+              result: result.object
+            });
+            eachCallback();
+          }
         }
-    })
+      );
+    },
+    function(err) {
+      if (err) {
+        callback(err);
+      } else {
+        console.log(execResult);
+        callback(null, execResult);
+      }
+    }
+  );
 };
