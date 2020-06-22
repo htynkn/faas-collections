0c389efd7bdec4078e1278aedf0f57b747004436
Huang YunKun <htynkn@gmail.com>
2020-06-22 22:28:03 +0800

    Update index.js

--- update-forked-repo/index.js
+++ update-forked-repo/index.js
@@ -1,7 +1,7 @@
 var request = require("request");
 var async = require("async");
 
-var targets = ["htynkn/spring-boot", "htynkn/dubbo", "htynkn/dubbo-samples","htynkn/slp-server-rust","htynkn/dubbo-admin"];
+var targets = ["htynkn/spring-boot", "htynkn/dubbo", "htynkn/dubbo-samples","htynkn/dubbo-admin"];
 
 module.exports.handler = function (event, context, callback) {
   var options = {
