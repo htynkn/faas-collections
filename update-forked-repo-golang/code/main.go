7de214680a99a54955f45f7c63318ee670b270ca
Huang Yunkun <htynkn@gmail.com>
2020-09-25 17:12:23 +0800

    update ignore config

--- update-forked-repo-golang/code/main.go
+++ update-forked-repo-golang/code/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"os"
 
 	gr "github.com/awesome-fc/golang-runtime"
+	"github.com/google/go-github/v32/github"
+	"golang.org/x/oauth2"
 )
 
 func handler(ctx *gr.FCContext, event []byte) ([]byte, error) {
@@ -12,6 +16,17 @@ func handler(ctx *gr.FCContext, event []byte) ([]byte, error) {
 	if err != nil {
 		fcLogger.Error("error:", err)
 	}
+
+	githubToken, _ := os.LookupEnv("GITHUB_TOKEN")
+	fcLogger.Infof(githubToken)
+
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: githubToken},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+
 	fcLogger.Infof("hello golang!")
 	return event, nil
 }
