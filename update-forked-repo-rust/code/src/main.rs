b33d1fb2f528ec8042652cd2ca95c66a892f1c22
htynkn <htynkn@gmail.com>
2021-10-27 14:16:28 +0800

    fix

--- update-forked-repo-rust/code/src/main.rs
+++ update-forked-repo-rust/code/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    // POST /invoke
+    let route = warp::path!("invoke")
+        .and(warp::post())
+        .map(|| "Hello, world!");
+
+    warp::serve(route).run(([0, 0, 0, 0], 9000)).await;
 }
